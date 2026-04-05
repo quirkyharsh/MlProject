676263ccb3d3bebf9698c0acd0197b2a5b3e26ae
quirkyharsh <harshpatil3.1428@gmail.com>
2026-04-05 09:33:27 +0530

    Project Data Transformataion using Pipeline

--- src/logger.py
+++ src/logger.py
@@ -3,16 +3,16 @@ import os
 from datetime import datetime 
 
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-log_path=os.path.join(os.getcwd(),"logs",LOG_FILE)
-os.makedirs(log_path,exist_ok=True)
 
-LOG_FILE_PATH = os.path.join(log_path,LOG_FILE)
+log_dir = os.path.join(os.getcwd(), "logs")
+os.makedirs(log_dir, exist_ok=True)
+
+LOG_FILE_PATH = os.path.join(log_dir, LOG_FILE)
 
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[%(acstime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    format="[%(asctime)s] %(lineno)d %(name)s - %(levelname)s - %(message)s",
     level=logging.INFO,
-
 )
 
 if __name__ == "__main__":
